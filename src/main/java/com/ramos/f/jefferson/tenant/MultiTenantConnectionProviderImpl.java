d4118c646d01ecefde1b2b853f55fbd7f7c95970
Jefferson Ramos <jefferson.f.ramos@hotmail.com>
2018-02-08 09:36:40 -0200

    Update MultiTenantConnectionProviderImpl.java

--- src/main/java/com/ramos/f/jefferson/tenant/MultiTenantConnectionProviderImpl.java
+++ src/main/java/com/ramos/f/jefferson/tenant/MultiTenantConnectionProviderImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ramos.f.jefferson.tenant;
 
 import javax.sql.DataSource;
@@ -11,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.lookup.DataSourceLookup;
 import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException;
 
-/**
- *
- * @author jeffe
- */
+
 public class MultiTenantConnectionProviderImpl extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl{
     
     @Autowired
